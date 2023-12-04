cc82c0538018e254ec6d7d11a337496ca95cf7df
erica <elhendricks@gmail.com>
2023-12-03 21:45:30 -0800

    result too low.  added new check. right before including digits at the end of the row.

--- day-03/app.js
+++ day-03/app.js
@@ -1,10 +1,9 @@
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('input.txt')
+    input: require('fs').createReadStream('test-cases/number-is-last-of-row.txt')
 });
 
 const data = []
-let partNumberIndices = []
-let symbolIndices = []
+let partNumberSum = 0
 
 lineReader.on('line', function (line) {
     // put textfile into data structure
@@ -15,22 +14,63 @@ lineReader.on('line', function (line) {
 lineReader.on('close', function () {
 
     data.forEach((row, rowIndex) => {
+        let numberStart, numberEnd, number = '', isPartNumber = false
+        let previousIsDigit = false
         row.forEach((char, colIndex) => {
-            if (isSymbol(char)) {
-                console.log(3, char)
-                findAndStoreAdjacentDigits(rowIndex, colIndex)
+
+            if (isDigit(char)) {
+                number += char
+                if (!numberStart) { numberStart = colIndex }
+                previousIsDigit = true
+
+                if (!isDigit(row[colIndex + 1])) {
+                    console.log('woohoo')
+                }
+            } else if (previousIsDigit) {
+                previousIsDigit = false
+                numberEnd = colIndex -1
+
+            
+                isPartNumber = (
+                    checkPartOfRowForSymbol(data[rowIndex -1], numberStart - 1, numberEnd + 1) ||
+                    checkPartOfRowForSymbol(data[rowIndex + 1], numberStart - 1, numberEnd + 1) ||
+                    isSymbol(row[numberStart - 1]) || 
+                    isSymbol(row[numberEnd + 1]) || 
+                    false
+                )
+
+                // Sum the part numbers
+                if (isPartNumber) {
+            
+                    partNumberSum += parseInt(number)   
+                    console.log(1, rowIndex +1, parseInt(number))
+
+                } else {
+                    // console.log(rowIndex, number)
+                }
+
+                // reset start, end, number
+                numberStart = ''
+                numberStart = ''
+                number = ''
+                isPartNumber = false
             }
         }) 
     })
+
+    console.log('Part number sum:', partNumberSum)
 });
 
 
 function isSymbol(char) {
+    if (typeof char !== 'string') {return false}
     // (not a '.' and not a digit)
     return char.match(/[^\.\d]/g)
 }
 
-function isNumber(char) {
+function isDigit(char) {
+    if (typeof char !== 'string') {return false}
+
     // digit
     return char.match(/\d/g)
 }
@@ -40,7 +80,7 @@ function findAndStoreAdjacentDigits(rowIndex, colIndex) {
         if (data[r]) {
             const row = data[r]
             for (let c = colIndex -1; c < colIndex + 2; c++) {
-                if (isNumber(row[c])) {
+                if (isDigit(row[c])) {
                     // store 
 
                     // rowIndex, // colIndex
@@ -51,6 +91,10 @@ function findAndStoreAdjacentDigits(rowIndex, colIndex) {
     }
 }
 
-function findFirstDigitOfNumber() {}
-function findLastDigitOfNumber() {}
-
+function checkPartOfRowForSymbol (row, startIndex, endIndex) {
+    if (!row) return false
+    for (i = startIndex; i <= endIndex; i++) {
+        if (isSymbol(row[i])) return true
+    }
+    return false
+}
